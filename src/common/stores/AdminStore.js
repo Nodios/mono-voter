be1f3cf6c15dadd6ad1140388e9ba2cf45068be3
Dino <drepac@mono-software.com>
2019-11-07 00:11:43 +0100

    admin changes

--- src/common/stores/AdminStore.js
+++ src/common/stores/AdminStore.js
@@ -1,48 +1,144 @@
-import { observable, action, runInAction } from 'mobx';
+import { observable, action, computed, runInAction } from 'mobx';
 import Firebase from '../firebase';
 
 const stepId = "k1UWLoF47u9t7jVu36aR";
 
+const defaultForm = {
+    question: '',
+    sortOrder: 0
+}
+
 class AdminStore {
     @observable step = 0;
+    @observable questions = [];
+
+    @observable form = defaultForm;
 
     _db = Firebase.firestore();
     _stepRef;
 
     constructor() {
+    }
 
+    @computed get displayStep() {
+        return this.step + 1;
     }
 
     @action.bound
     async init() {
+        const auth = await Firebase.auth().signInAnonymously();
+
         this._stepRef = this._db.collection('steps').doc(stepId);
 
-        let step = await this._stepRef.get();
-        if (step.exists) {
-            runInAction(() => {
-                const stepObj = step.data();
-                this.step = stepObj.questionStep;
-            });
-        }
+        this._stepRef.onSnapshot(doc => {
+            const stepObj = doc.data();
 
+            runInAction(() => {
+                this.setStep(stepObj.questionStep);
+            });
+        });
+
+        await this.fetchQuestions();
+    }
+
+    @action.bound
+    setQuestion(question) {
+        this.question = question;
+    }
+
+    @action.bound
+    setStep(step) {
+        this.step = step;
     }
 
     @action.bound
     async next() {
         const increment = Firebase.firestore.FieldValue.increment(1);
 
-        await this._stepRef.update({ questionStep: increment });
+        let newStep = this.step;
+        try {
+            await this._stepRef.update({ questionStep: increment });
+            newStep++;
 
-        this.step++;
+            const question = await this.fetchQuestion(newStep);
+
+            runInAction(() => {
+                this.step = newStep;
+                this.question = question;
+            });
+        }
+        catch (e) {
+
+        }
     }
 
     @action.bound
     async previous() {
         if (this.step > 0) {
             const decrement = Firebase.firestore.FieldValue.increment(-1);
-            await this._stepRef.update({ questionStep: decrement });
-            this.step--;
+
+            let newStep = this.step;
+            try {
+                await this._stepRef.update({ questionStep: decrement });
+                newStep--;
+
+                const question = await this.fetchQuestion(newStep);
+
+                runInAction(() => {
+                    this.step = newStep;
+                    this.question = question;
+                });
+            }
+            catch (e) {
+
+            }
         }
+    }
+
+    // FORM
+    @action.bound
+    updateField(event) {
+        const { name, value } = event.target;
+
+        this.form[name] = value;
+    }
+
+    @action.bound
+    async submit(event) {
+        event.preventDefault();
+
+        await this.addQuestion(this.form);
+
+        runInAction(async () => {
+            this.form = defaultForm;
+            await this.fetchQuestions();
+        });
+
+    }
+
+    addQuestion = async (questionObj) => {
+        // this generates new document
+        const newItemRef = await Firebase.firestore().collection('questions').doc();
+
+        // set new data
+        await newItemRef.set({
+            question: questionObj.question,
+            sortOrder: questionObj.sortOrder,
+            votes: {}
+        })
+    }
+
+    fetchQuestions = async () => {
+        this.questions = [];
+        const result = await Firebase.firestore().collection('questions')
+            .orderBy('sortOrder', 'asc')
+            .get();
+
+        runInAction(() => {
+            result.forEach(doc => {
+                this.questions.push(doc.data());
+            });
+        });
     }
 }
 
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
-import { Container, Typography, Card, CardHeader, CardContent, CardActions, Button } from '@material-ui/core';
-import { ArrowBack, ArrowForward } from '@material-ui/icons';
+import {
+    Container,
+    Typography,
+    Card,
+    CardHeader,
+    CardContent,
+    CardActions,
+    Button,
+    TextField,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    withStyles
+} from '@material-ui/core';
+import { ArrowBack, ArrowForward, Visibility } from '@material-ui/icons';
 
 import { AdminStore } from '../common/stores';
+
+const styles = theme => ({
+    card: {
+        marginTop: theme.spacing(2)
+    }
+});
 
 @inject(i => {
     const viewStore = new AdminStore();
@@ -18,18 +38,26 @@ class AdminPage extends React.Component {
         await this.props.viewStore.init();
     }
     render() {
-        const { viewStore } = this.props;
-        const { step, next, previous } = viewStore;
+        const { viewStore, classes } = this.props;
+        const { step, displayStep, next, previous, questions, form, updateField, submit } = viewStore;
         return (
             <Container>
-                <Typography variant="h5" component="h3">
-                    Admin page
-                </Typography>
-                <Card>
-                    <CardHeader title="Step" subheader="Use this to change question" />
+                <Card className={classes.card}>
                     <CardContent>
-                        Current question: {step}
+                        <Typography variant="h5" component="h3">
+                            Admin page
+                        </Typography>
                     </CardContent>
+                </Card>
+                <Card className={classes.card}>
+                    <CardHeader title="Questions management" subheader="Use this to change question" />
+                    {/* <CardContent>
+                        {question &&
+                            <Typography variant="body2" component="p">
+                                {displayStep}. {question.question}
+                            </Typography>
+                        }
+                    </CardContent> */}
                     <CardActions>
                         <Button
                             variant="contained"
@@ -50,9 +78,59 @@ class AdminPage extends React.Component {
                         </Button>
                     </CardActions>
                 </Card>
+                <Card className={classes.card}>
+                    <CardHeader title="List of questions" />
+                    <CardContent>
+                        <List>
+                            {questions.map((q, idx) => {
+                                // <Typography variant="body2" component="p">{q.sortOrder + 1}. {q.question}</Typography>
+                                return (
+                                    <ListItem key={idx}>
+                                        {q.sortOrder === step && <ListItemIcon>
+                                            <Visibility />
+                                        </ListItemIcon>}
+                                        <ListItemText primary={q.question} secondary={"Sort order: " + q.sortOrder} />
+                                    </ListItem>
+                                )
+                            })}
+                        </List>
+                    </CardContent>
+                </Card>
+                <Card className={classes.card}>
+                    <CardHeader title="Add new question" />
+                    <CardContent>
+                        <form onSubmit={submit} noValidate autoComplete="off">
+                            <div>
+                                <TextField
+                                    required
+                                    name="question"
+                                    label="Question"
+                                    margin="normal"
+                                    value={form.question}
+                                    onChange={updateField}
+                                />
+                            </div>
+                            <div>
+                                <TextField
+                                    required
+                                    type="number"
+                                    name="sortOrder"
+                                    label="Sort order"
+                                    margin="normal"
+                                    value={form.sortOrder}
+                                    onChange={updateField}
+                                    inputProps={{
+                                        min: 1
+                                    }}
+                                />
+                                <Button type="submit">Add question</Button>
+                            </div>
+                        </form>
+                    </CardContent>
+                </Card>
             </Container>
         );
     }
 }
 
-export default AdminPage;
+export default withStyles(styles)(AdminPage);
