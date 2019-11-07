cdb7fc9d3f4a6948de0d1e036c14b4789f2cd749
Dino <drepac@mono-software.com>
2019-11-07 22:05:37 +0100

    fix route constraint

---
+++ build/precache-manifest.691ac436551e83bb5a8381da32feb67e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3cbc19e6dad70a701dbdf7eefd579c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "561338f0c403018b4b4f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6fab06ef644f7699dd1e",
+    "url": "/static/js/2.9f601f3e.chunk.js"
+  },
+  {
+    "revision": "561338f0c403018b4b4f",
+    "url": "/static/js/main.1a949974.chunk.js"
+  },
+  {
+    "revision": "2df212be51ddd40f3357",
+    "url": "/static/js/runtime-main.0bb7170f.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4718f73933ef4b1c3ed02753c57d31c2.js"
+  "/precache-manifest.691ac436551e83bb5a8381da32feb67e.js"
 );
 
 self.addEventListener('message', (event) => {
